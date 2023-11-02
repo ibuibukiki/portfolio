497d369a2bea062dcfe5db7714c4c3af59ec0958
ibuibukiki <63579269+ibuibukiki@users.noreply.github.com>
2023-11-02 18:58:58 +0900

    Revert "Revert "[update] リンクを更新""

--- lib/screen/oko_screen.dart
+++ lib/screen/oko_screen.dart
@@ -93,7 +93,7 @@ class OkoScreen extends HookConsumerWidget {
                       ),
                       const SizedBox(height: 24),
                       const Text(
-                        "Android版 リリース済み",
+                        "Android・ios リリース済み",
                         style: TextStyle(
                           fontSize: 14,
                           fontWeight: FontWeight.w900,
